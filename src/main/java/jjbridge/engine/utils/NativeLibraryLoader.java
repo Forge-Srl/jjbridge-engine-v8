deca99b5b43f29f44f8d37459410ee4c684c9c04
Tommaso Bossi <tommasobossi95@gmail.com>
2021-07-20 14:51:33 +0200

    chore: remove unused import

--- src/main/java/jjbridge/engine/utils/NativeLibraryLoader.java
+++ src/main/java/jjbridge/engine/utils/NativeLibraryLoader.java
@@ -3,7 +3,6 @@ package jjbridge.engine.utils;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
